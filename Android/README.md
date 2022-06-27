4a680d2f814bb021bf09a88ca05c661301aac6b7
chenjinyan.jimmy <chenjinyan.jimmy@bytedance.com>
2022-06-27 10:33:02 +0800

    New project

---
+++ Android/README.md
@@ -0,0 +1,57 @@
+﻿# veGame SDK Demo
+
+## 说明
+
+本项目是火山引擎云游戏客户端 SDK 的快速演示 demo。获取项目以后，开发者可以快速构建应用，体验云游戏服务的主要功能；也能参考其中的代码，在实际的客户端应用中实现相似的功能。
+
+## 环境要求
+
+### 硬件要求
+
+支持 Android 4.3（Android-19+）及以上系统的真机设备，支持 armeabi-v7a。
+
+### 软件要求
+
+- IDE：Android Studio（推荐使用最新版本）
+- 搭建 Java 环境，使用 Java 作为开发语言
+
+## 快速开始
+
+1. 克隆或下载 demo 源文件到本地；
+2. 在 Android Studio 中引入项目；运行工程文件，编译生成 APK 文件，在 Android 设备上运行预览；
+3. 相关的运行信息，在打印的log中查看。
+
+## 目录结构
+
+```
+├── main
+│       │   ├── AndroidManifest.xml
+│       │   ├── java
+│       │   │   └── com
+│       │   │       └── volcengine
+│       │   │           └── vegameengine
+│       │   │               ├── GameActivity.java  //显示游戏的Activity
+│       │   │               ├── GsonConverter.java  // 用于SDK 传入的JSON转换的实现 
+│       │   │               ├── InitApplication.java // 工程的Application 负责初始化SDK等
+│       │   │               ├── MainActivity.kt // 首页指定 gameId、ak、sk、token
+│       │   │               ├── ui
+│       │   │               │   └── MessageChannelDialog.java
+│       │   │               └── util
+│       │   │                   ├── DialogUtils.java
+│       │   │                   ├── ScreenUtil.java  //屏幕工具类用于适配挖孔屏
+│       │   │                   └── SizeUtils.java
+     
+```
+
+## 接入的流程
+
+1. 在 Application 中可以选择初始化一些埋点信息；
+2. 在 MainActivity 中填写游戏的 gameId、ak、sk 和 token，并初始化云游戏 SDK 实例（`veGameEngine`）；
+3. 初始化成功后，调用 `start()` 接口开始播放；
+4. 切到后台，可以选择调用 `pause()` 接口停止音视屏流；
+5. 切回前台，可以选择调用 `resume()` 接口恢复音视频流；
+6. 结束，调用`stop()` 接口结束游戏并释放资源。
+
+## 参考资料
+
+火山引擎云游戏产品说明文档：https://www.volcengine.com/docs/6512
---
+++ Android/app/src/main/java/com/volcengine/vegameengine/GsonConverter.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2022 Volcengine
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.volcengine.vegameengine;
+
+import com.google.gson.Gson;
+import com.google.gson.internal.Primitives;
+import com.google.gson.stream.JsonReader;
+import com.volcengine.androidcloud.common.api.IJsonConverter;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+
+public class GsonConverter implements IJsonConverter {
+    Gson gson;
+
+
+    public GsonConverter() {
+        gson = new Gson();
+    }
+
+    @Override
+    public <T> T fromJson(String json, Class<T> cls) {
+        if (json == null) {
+            return null;
+        }
+        try {
+            return gson.fromJson(json, cls);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public <T> T fromJson(InputStream json, Class<T> cls) {
+        try {
+            JsonReader reader = new JsonReader(new InputStreamReader(json));
+            return Primitives.wrap(cls).cast(gson.fromJson(reader, cls));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public <T> String toJson(T object) {
+        if (object == null) {
+            return null;
+        }
+        return gson.toJson(object);
+    }
+
+    @Override
+    public <T> T fromJson(String json, Type type) {
+        try {
+            return gson.fromJson(json, type);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
