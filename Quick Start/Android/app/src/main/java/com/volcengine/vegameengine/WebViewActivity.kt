dfd24931767e8fb731d8ffd511f6b0deaaedbf29
jincan.2010 <jincan.2010@bytedance.com>
2022-09-27 14:49:10 +0800

    webview增加进度条

--- Quick Start/Android/app/src/main/java/com/volcengine/vegameengine/WebViewActivity.kt
+++ Quick Start/Android/app/src/main/java/com/volcengine/vegameengine/WebViewActivity.kt
@@ -1,21 +1,37 @@
 package com.volcengine.vegameengine
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebView
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+
 
 class WebViewActivity : AppCompatActivity() {
 
     private lateinit var webView: WebView
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
         webView = findViewById(R.id.web_view)
-        intent.getStringExtra("uri")?.let { webView.loadUrl(it) }
+        progressBar = findViewById(R.id.progress_bar)
+        intent.getStringExtra("uri")?.let {
+            webView.webChromeClient = object : WebChromeClient() {
+                override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                    if (newProgress == 100) {
+                        progressBar.visibility = View.GONE
+                    }
+                    else {
+                        progressBar.visibility = View.VISIBLE
+                        progressBar.progress = newProgress
+                    }
+                }
+            }
+            webView.loadUrl(it)
+        }
     }
 
 
