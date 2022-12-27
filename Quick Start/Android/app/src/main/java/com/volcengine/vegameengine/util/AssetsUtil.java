73ba5a067c1b75d6206e36cce4edad5c2d15b145
wangchenteng <wangchenteng@bytedance.com>
2022-12-27 22:10:56 +0800

    feat: update Message Channel callback

---
+++ Quick Start/Android/app/src/main/java/com/volcengine/vegameengine/util/AssetsUtil.java
@@ -0,0 +1,36 @@
+package com.volcengine.vegameengine.util;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * @date 2022/12/27
+ * @desc
+ */
+public class AssetsUtil {
+    private static final String ENCODING = "UTF-8";
+
+    public static InputStream getFileFromAssets(Context context, String fileName) throws IOException {
+        AssetManager am = context.getAssets();
+        return am.open(fileName);
+    }
+
+    public static String getTextFromAssets(Context context, String fileName) {
+        String result = null;
+        try {
+            InputStream in = getFileFromAssets(context, fileName);
+            int length = in.available();
+            byte[] buffer = new byte[length];
+            in.read(buffer);
+            result = new String(buffer, Charset.forName(ENCODING));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
