7466367dec16c5b541ca70f342233d940b669137
yinhaichao <yinhaichao@bytedance.com>
2024-03-26 15:34:58 +0800

    feat：添加摇杆逻辑

---
+++ QuickStart/iOS/VeGameDemo/GamePad/VeGamePadView.h
@@ -0,0 +1,19 @@
+//
+//  VeGamePadView.h
+//  VePlayer
+//
+//  Created by yinhaichao on 2024/2/1.
+//  Copyright © 2020 ByteDance Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class VeGamePadItem;
+
+@interface VeGamePadItemView : UIView
+
+@end
+
+@interface VeGamePadJoystickView : VeGamePadItemView
+
+
+@end
