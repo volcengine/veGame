1ba55a4edb027ba89f383a9e6240e33b8ebdaf56
yinhaichao <yinhaichao@bytedance.com>
2024-05-15 18:12:34 +0800

    fix: 添加VEGame头文件

---
+++ QuickStart/iOS/VEGameDESC.h
@@ -0,0 +1,18 @@
+//
+//  Header.h
+//  VeGamePublicDemo
+//
+//  Created by Haichao Yin on 15/5/2024.
+//  Copyright © 2024 Haichao Yin. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+/**
+ 火山引擎云游戏结合云计算和超低延迟音视频传输技术、灵活的资源调度方案、多终端跨平台适配的 SDK 和 OpenAPI 接口，为客户提供全方位整体云游戏技术解决方案。
+
+ 开发者无需适配不同的软硬件平台，即可将游戏部署到服务端提供云游戏服务。本仓库提供 Android、iOS、Web/H5 客户端 Quick Start Demo 工程文件下载，帮助开发者快速搭建示例，体验云游戏服务。
+ */
+
+#endif /* Header_h */
